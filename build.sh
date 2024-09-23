ee0f7b148e5b86cd01dfd90ba48501238b82ee7c
Raphaël <raphael.mabille@epitech.eu>
2024-09-23 08:48:56 +0000

    build(script): add conan detect profile in case it's the first conan use

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+conan profile detect --force
 conan install . --output-folder=build --build=missing -c "tools.system.package_manager:mode=install" -c "tools.system.package_manager:sudo=true"
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
