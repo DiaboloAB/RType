2a4d16e3e0fd646a93fcf66b7f14bc6063618ba1
Raphaël <raphael.mabille@epitech.eu>
2024-09-23 07:26:16 +0000

    build(conan): add script to automate the conan commands

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-conan install . --output-folder=build --build=missing
+conan install . --output-folder=build --build=missing -c "tools.system.package_manager:mode=install" -c "tools.system.package_manager:sudo=true"
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,5 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMakeDeps
-from conan.tools.system.package_manager import PacMan
 
 class MyProjectConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -9,10 +8,6 @@ class MyProjectConan(ConanFile):
     def requirements(self):
         self.requires("asio/1.31.0")
         self.requires("glfw/3.4")
-
-    def system_requirements(self):
-        pacman = PacMan(self)
-        pacman.install(["libxcb", "libfontenc", "libice", "libsm", "libxaw", "libxcomposite", "libxcursor"], update=True, check=True)
 
     def build_requirements(self):
         self.tool_requires("cmake/3.22.6")
