0c89b02a3f4ec3de02c4549f53e52144860089f8
Raphaël <raphael.mabille@epitech.eu>
2024-09-24 09:10:33 +0000

    build(cmake): comment libs requirement to be added later

--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,9 @@
  **********************************************************************************/
 
 #include <iostream>
-#include <GLFW/glfw3.h>
-
-#include <SFML/Audio.hpp>
-#include <SFML/Graphics.hpp>
 
 int main()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
- 
-        // Clear screen
-        window.clear();
-        // Update the window
-        window.display();
-    }
- 
-    return EXIT_SUCCESS;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
