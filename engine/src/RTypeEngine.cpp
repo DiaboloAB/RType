91e0405fe97d65e7215664be7f5c86b071bc23a8
Diabolo <alexis.boitel@epitech.eu>
2024-09-24 14:25:34 +0200

    feat(ecs): simple component manager

--- engine/src/RTypeEngine.cpp
+++ engine/src/RTypeEngine.cpp
@@ -18,3 +18,30 @@ Engine::~Engine()
 {
     // Destructor implementation
 }
+
+struct Position
+{
+    float x;
+    float y;
+
+    Position(float x, float y) : x(x), y(y) {}
+};
+
+void Engine::run()
+{
+    ECS::Entity entity = _registry.create();
+    _registry.emplace<Position>(entity, 0.0f, 0.0f);
+    Position &pos = _registry.getComponent<Position>(entity);
+
+    std::cout << "Position: " << pos.x << ", " << pos.y << std::endl;
+    int i = 0;
+    while (i < 10)
+    {
+        pos.x += 1.0f;
+        pos.y += 1.0f;
+        std::cout << "Position: " << pos.x << ", " << pos.y << std::endl;
+        i++;
+    }
+    Position &pos2 = _registry.getComponent<Position>(entity);
+    std::cout << "Position: " << pos2.x << ", " << pos2.y << std::endl;
+}
