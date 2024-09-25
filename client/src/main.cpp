3b81bce1ce861d56c56a16d95da078b1a535c056
mlargeot <morgan.largeot@epitech.eu>
2024-09-25 11:34:57 +0200

    feat(client/src/main): add client main that run its engine

---
+++ client/src/main.cpp
@@ -0,0 +1,17 @@
+/**********************************************************************************
+ * Project: RType
+ * Description: A GAME ENGINE THAT ROARS !
+ * Author: Alexis BOITEL, Raphael MABILLE, Morgan LARGEOT, Maxence LARGEOT, Arthur DORIEL
+ * Date, Location: 2024, Rennes
+ **********************************************************************************/
+
+#include "RTypeEngine.hpp"
+#include <string>
+#include <iostream>
+
+int main()
+{
+    RType::Engine engine;
+    engine.run();
+    return 0;
+}
