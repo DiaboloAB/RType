a307c984cbb8156526e5a1ebcbc73b7f06a52cd6
mlargeot <morgan.largeot@epitech.eu>
2024-10-01 00:40:31 +0200

    feat(engine/NetworkHandler.hpp): add handler & exceptions handler

--- engine/src/NetworkHandler/NetworkHandler.hpp
+++ engine/src/NetworkHandler/NetworkHandler.hpp
@@ -27,6 +27,18 @@ namespace RType::Network
         public:
             void sendData();
             void receiveData();
+            void handleData(std::vector<char> recvBuffer, asio::ip::udp::endpoint remoteEndpoint);
+
+        public:
+            class NetworkHandlerError : public std::exception {
+                public:
+                    NetworkHandlerError(std::string msg) : _msg(msg) {};
+                    ~NetworkHandlerError() {};
+                private:
+                    const char *what() const noexcept override {return _msg.c_str();};
+                private:
+                    std::string _msg;
+            };
 
         private:
             std::string _host = "";
@@ -35,6 +47,6 @@ namespace RType::Network
             std::list<asio::ip::udp::endpoint> _endpointList = {};
             asio::io_context _io_context;
             std::shared_ptr<asio::ip::udp::socket> _socket = nullptr;
-            // recieve buffer
+            std::vector<char> _recvBuffer;
     };
 }
