6fbd4cfd15e524e84b28afe01ea12a166f3f990d
mlargeot <morgan.largeot@epitech.eu>
2024-10-08 10:53:21 +0200

    feat(NetworkSystem): add packet handling

--- engine/src/common/systems/NetworkSystem.hpp
+++ engine/src/common/systems/NetworkSystem.hpp
@@ -16,36 +16,75 @@ namespace RType
 {
 
 class NetworkSystem : public ISystem
-{
+{   
    public:
-    NetworkSystem() {}
-    ~NetworkSystem() {}
-
-
-    void manageQueue(mobs::Registry &registry, GameContext &gameContext)
-    {
-        std::queue<std::pair<std::shared_ptr<RType::Network::APacket>, asio::ip::udp::endpoint>>
-            packetQueue = gameContext._networkHandler->getPacketQueue();
-
-        while (!packetQueue.empty()) {
-            std::shared_ptr<RType::Network::APacket> packet = packetQueue.front().first;
-
-            // handle le packet en fonction de son type
-
-            packetQueue.pop();
-            gameContext._networkHandler->popQueue();
+        NetworkSystem()
+        {
+            this->_systemsMap[Network::HISERVER] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
+            this->_systemsMap[Network::HICLIENT] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
+            this->_systemsMap[Network::ADIOSSERVER] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
+            this->_systemsMap[Network::PING] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
+            this->_systemsMap[Network::CREATEENTITY] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
+            this->_systemsMap[Network::DESTROYENTITY] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
+            this->_systemsMap[Network::MOVEENTITY] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
+            this->_systemsMap[Network::UPDATEENTITY] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
+            this->_systemsMap[Network::CLIENTEVENT] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
+            this->_systemsMap[Network::KICKCLIENT] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
+            this->_systemsMap[Network::PACKETVALIDATION] = [](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+            { return; };
         }
-        return;
-    }
 
-    void update(mobs::Registry &registry, GameContext &gameContext) override
-    {
-        if (gameContext._networkHandler == nullptr)
+        ~NetworkSystem() {}
+
+
+        void manageQueue(mobs::Registry &registry, GameContext &gameContext)
+        {
+            std::queue<std::pair<std::shared_ptr<Network::APacket>, asio::ip::udp::endpoint>>
+                packetQueue = gameContext._networkHandler->getPacketQueue();
+
+            std::shared_ptr<Network::APacket> packet;
+            Network::PacketType type;
+
+            while (!packetQueue.empty()) {
+                try {
+                    packet = packetQueue.front().first;
+                    type = packet->getPacketType();
+                } catch (std::exception &e) {
+                    std::cerr << "[manageQueue ERROR] Unable to get packet or packetType" << std::endl;
+                    return;
+                } 
+
+                try {
+                    packetQueue.pop();
+                    gameContext._networkHandler->popQueue();
+                } catch (std::exception &e) {
+                    std::cerr << "[manageQueue ERROR] Problem when removing packets from queue." << std::endl;
+                    return;
+                }
+            }
             return;
-        this->manageQueue(registry, gameContext);
-    }
+        }
+
+        void update(mobs::Registry &registry, GameContext &gameContext) override
+        {
+            if (gameContext._networkHandler == nullptr)
+                return;
+            this->manageQueue(registry, gameContext);
+        }
 
    private:
+        std::unordered_map<Network::PacketType, std::function<void(std::shared_ptr<Network::APacket> &,
+            mobs::Registry &, GameContext &)>> _systemsMap;
 };
 }
 
