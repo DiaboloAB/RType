e71f1f3f1a33b4869cb631882cb7da8f309f6139
mlargeot <morgan.largeot@epitech.eu>
2024-10-08 13:42:16 +0200

    feat(NetworkSystem): add all packets handler

--- engine/src/common/systems/NetworkSystem.hpp
+++ engine/src/common/systems/NetworkSystem.hpp
@@ -20,28 +20,28 @@ class NetworkSystem : public ISystem
    public:
         NetworkSystem()
         {
-            this->_systemsMap[Network::HISERVER] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { handleHiServer(packet, registry, gameContext); };
-            this->_systemsMap[Network::HICLIENT] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { handleHiClient(packet, registry, gameContext); };
-            this->_systemsMap[Network::ADIOSSERVER] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { return; };
-            this->_systemsMap[Network::PING] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { return; };
-            this->_systemsMap[Network::CREATEENTITY] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { return; };
-            this->_systemsMap[Network::DESTROYENTITY] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { return; };
-            this->_systemsMap[Network::MOVEENTITY] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { return; };
-            this->_systemsMap[Network::UPDATEENTITY] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { return; };
-            this->_systemsMap[Network::CLIENTEVENT] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { return; };
-            this->_systemsMap[Network::KICKCLIENT] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { return; };
-            this->_systemsMap[Network::PACKETVALIDATION] = [this](std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
-            { return; };
+            this->_systemsMap[Network::HISERVER] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handleHiServer(packet, sender, registry, gameContext); };
+            this->_systemsMap[Network::HICLIENT] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handleHiClient(packet, sender, registry, gameContext); };
+            this->_systemsMap[Network::ADIOSSERVER] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handleAdiosServer(packet, sender, registry, gameContext); };
+            this->_systemsMap[Network::PING] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handlePing(packet, sender, registry, gameContext); };
+            this->_systemsMap[Network::CREATEENTITY] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handleCreateEntity(packet, sender, registry, gameContext); };
+            this->_systemsMap[Network::DESTROYENTITY] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handleDestroyEntity(packet, sender, registry, gameContext); };
+            this->_systemsMap[Network::MOVEENTITY] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handleMoveEntity(packet, sender, registry, gameContext); };
+            this->_systemsMap[Network::UPDATEENTITY] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handleUpdateEntity(packet, sender, registry, gameContext); };
+            this->_systemsMap[Network::CLIENTEVENT] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handleClientEvent(packet, sender, registry, gameContext); };
+            this->_systemsMap[Network::KICKCLIENT] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handleKickClient(packet, sender, registry, gameContext); };
+            this->_systemsMap[Network::PACKETVALIDATION] = [this](std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+            { handlePacketValidation(packet, sender, registry, gameContext); };
         }
 
         ~NetworkSystem() {}
@@ -50,11 +50,12 @@ class NetworkSystem : public ISystem
         * @brief HiServer packet handler.
         *
         * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
         * @param registry: Entity handler.
         * @param gameContext: Object that allow access to engine attributes & methods.
         * 
         */
-        void handleHiServer (std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+        void handleHiServer (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
         {
             return;
         }
@@ -63,11 +64,138 @@ class NetworkSystem : public ISystem
         * @brief HiClient packet handler.
         *
         * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
         * @param registry: Entity handler.
         * @param gameContext: Object that allow access to engine attributes & methods.
         * 
         */
-        void handleHiClient (std::shared_ptr<Network::APacket> &packet, mobs::Registry &registry, GameContext &gameContext)
+        void handleHiClient (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+        {
+            return;
+        }
+
+        /**
+        * @brief AdiosServer packet handler.
+        *
+        * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
+        * @param registry: Entity handler.
+        * @param gameContext: Object that allow access to engine attributes & methods.
+        * 
+        */
+        void handleAdiosServer (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+        {
+            return;
+        }
+
+        /**
+        * @brief Ping packet handler.
+        *
+        * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
+        * @param registry: Entity handler.
+        * @param gameContext: Object that allow access to engine attributes & methods.
+        * 
+        */
+        void handlePing (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+        {
+            return;
+        }
+
+        /**
+        * @brief CreateEntity packet handler.
+        *
+        * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
+        * @param registry: Entity handler.
+        * @param gameContext: Object that allow access to engine attributes & methods.
+        * 
+        */
+        void handleCreateEntity (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+        {
+            return;
+        }
+
+        /**
+        * @brief DestroyEntity packet handler.
+        *
+        * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
+        * @param registry: Entity handler.
+        * @param gameContext: Object that allow access to engine attributes & methods.
+        * 
+        */
+        void handleDestroyEntity (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+        {
+            return;
+        }
+
+        /**
+        * @brief MoveEntity packet handler.
+        *
+        * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
+        * @param registry: Entity handler.
+        * @param gameContext: Object that allow access to engine attributes & methods.
+        * 
+        */
+        void handleMoveEntity (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+        {
+            return;
+        }
+
+        /**
+        * @brief UpdateEntity packet handler.
+        *
+        * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
+        * @param registry: Entity handler.
+        * @param gameContext: Object that allow access to engine attributes & methods.
+        * 
+        */
+        void handleUpdateEntity (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+        {
+            return;
+        }
+
+        /**
+        * @brief ClientEvent packet handler.
+        *
+        * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
+        * @param registry: Entity handler.
+        * @param gameContext: Object that allow access to engine attributes & methods.
+        * 
+        */
+        void handleClientEvent (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+        {
+            return;
+        }
+
+        /**
+        * @brief KickClient packet handler.
+        *
+        * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
+        * @param registry: Entity handler.
+        * @param gameContext: Object that allow access to engine attributes & methods.
+        * 
+        */
+        void handleKickClient (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
+        {
+            return;
+        }
+
+        /**
+        * @brief PacketValidation packet handler.
+        *
+        * @param packet: Packet received.
+        * @param sender: Endpoint of the packet sender.
+        * @param registry: Entity handler.
+        * @param gameContext: Object that allow access to engine attributes & methods.
+        * 
+        */
+        void handlePacketValidation (std::shared_ptr<Network::APacket> &packet, asio::ip::udp::endpoint &sender, mobs::Registry &registry, GameContext &gameContext)
         {
             return;
         }
@@ -85,11 +213,13 @@ class NetworkSystem : public ISystem
                 packetQueue = gameContext._networkHandler->getPacketQueue();
 
             std::shared_ptr<Network::APacket> packet;
+            asio::ip::udp::endpoint sender;
             Network::PacketType type;
 
             while (!packetQueue.empty()) {
                 try {
                     packet = packetQueue.front().first;
+                    sender = packetQueue.front().second;
                     type = packet->getPacketType();
                 } catch (std::exception &e) {
                     std::cerr << "[manageQueue ERROR] Unable to get packet or packetType" << std::endl;
@@ -123,7 +253,7 @@ class NetworkSystem : public ISystem
         }
 
    private:
-        std::unordered_map<Network::PacketType, std::function<void(std::shared_ptr<Network::APacket> &,
+        std::unordered_map<Network::PacketType, std::function<void(std::shared_ptr<Network::APacket> &, asio::ip::udp::endpoint &,
             mobs::Registry &, GameContext &)>> _systemsMap;
 };
 }
