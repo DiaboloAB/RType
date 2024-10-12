e0fa1673f4e8eacb632a6af0e4b0af9196de1802
maxence <maxence.largeot@epitech.eu>
2024-10-12 16:58:59 +0200

    feat(feature/clientEvents): Add stop event

---
+++ engine/src/NetworkEvents/StopMoveUpEvent.hpp
@@ -0,0 +1,41 @@
+/**********************************************************************************
+ * Project: RType
+ * Description: A GAME ENGINE THAT ROARS !
+ * Author: Alexis BOITEL, Raphael MABILLE, Morgan LARGEOT, Maxence LARGEOT, Arthur DORIEL
+ * Date, Location: 2024, Rennes
+ **********************************************************************************/
+
+#pragma once
+
+#include <asio.hpp>
+#include <mobs/mobs.hpp>
+#include <gameContext/GameContext.hpp>
+#include <NetworkHandler/NetworkIdHandler.hpp>
+
+namespace RType::Network {
+    class StopMoveUpEvent {
+        public:
+            static bool checkSenderValidity(asio::ip::udp::endpoint &sender, GameContext &gameContext)
+            {
+                auto networkHandler = gameContext._networkHandler;
+                if (!networkHandler || !networkHandler->getIsServer() ||
+                    networkHandler->getGameState() == IN_MENU)
+                    return false;
+                auto endpointMap = networkHandler->getEndpointMap();
+                auto senderData = endpointMap.find(sender);
+                if (senderData == endpointMap.end() || !senderData->second.getConnected()) return false;
+                return true;
+            }
+        
+            static void update(asio::ip::udp::endpoint &sender, mobs::Registry &registry,
+                        GameContext &gameContext, NetworkIdHandler &idHandler)
+            {
+                if (!checkSenderValidity(sender, gameContext)) return;
+                auto &networkHandler = gameContext._networkHandler;
+                auto senderData = networkHandler->getEndpointMap().find(sender);
+                if (senderData->second.getDirY() > 0.0)
+                    return;
+                senderData->second.setDirY(senderData->second.getDirY() + 1.0);
+            }
+    };
+}
