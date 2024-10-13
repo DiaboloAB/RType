0aa93aee64c1303cfb1b59ea510b8c6cb783bf07
maxence <maxence.largeot@epitech.eu>
2024-10-13 10:45:15 +0200

    refactor(feature/clientEvents): fix move err

--- engine/src/common/cppScripts/MovePlayer.hpp
+++ engine/src/common/cppScripts/MovePlayer.hpp
@@ -22,24 +22,24 @@ class MovePlayerScript : public RType::ICppScript
         auto &transform = registry.get<Transform>(_entity);
         auto &hitbox = registry.get<Hitbox>(_entity);
         int speed = 600;
-        if (gameContext._runtime->getKeyDown(KeyCode::UpArrow))
+        if (gameContext._runtime->getKey(KeyCode::UpArrow))
         {
             transform.position.y -= speed * gameContext._deltaT;
             if (transform.position.y < 0) transform.position.y = 0;
         }
-        if (gameContext._runtime->getKeyDown(KeyCode::DownArrow))
+        if (gameContext._runtime->getKey(KeyCode::DownArrow))
         {
             transform.position.y += speed * gameContext._deltaT;
             if (transform.position.y > 1080 - hitbox.size.y)
                 transform.position.y = 1080 - hitbox.size.y;
         }
-        if (gameContext._runtime->getKeyDown(KeyCode::RightArrow))
+        if (gameContext._runtime->getKey(KeyCode::RightArrow))
         {
             transform.position.x += speed * gameContext._deltaT;
             if (transform.position.x > 1920 - hitbox.size.x)
                 transform.position.x = 1920 - hitbox.size.x;
         }
-        if (gameContext._runtime->getKeyDown(KeyCode::LeftArrow))
+        if (gameContext._runtime->getKey(KeyCode::LeftArrow))
         {
             transform.position.x -= speed * gameContext._deltaT;
             if (transform.position.x < 0) transform.position.x = 0;
