2702770c232aae4981bcf6e7eca6f0c354cf4408
MaxenceLgt <maxence.largeot@epitech.eu>
2024-10-20 19:06:17 +0200

    refactor(feature/dimensionLib): fix client socket init

--- libs/dimension/src/dimension/endpoint/client/Client.cpp
+++ libs/dimension/src/dimension/endpoint/client/Client.cpp
@@ -21,30 +21,20 @@ namespace dimension {
 
     void DimensionClient::connectServer(std::string host, unsigned int port)
     {
+        if (this->_io_context != nullptr)
+            return;
         try {
             this->_io_context = std::make_shared<asio::io_context>();
             asio::ip::udp::endpoint endpoint(asio::ip::udp::v4(), 0);
-            if (!this->_io_context)
-                std::cerr << "io null" << std::endl;
-            std::cerr << endpoint.address() << " " << endpoint.port() << std::endl;
-            std::cerr << "SEGV" << std::endl;
             this->_socket = std::make_shared<asio::ip::udp::socket>(*this->_io_context, endpoint);
-            std::cerr << "SEGV" << std::endl;
             asio::ip::udp::resolver resolver(*this->_io_context);
-            std::cerr << "SEGV" << std::endl;
             asio::ip::udp::resolver::results_type endpoints = resolver.resolve(asio::ip::udp::v4(), host, std::to_string(port));
-            std::cerr << "SEGV" << std::endl;
             this->_serverEndpoint = std::make_shared<asio::ip::udp::endpoint>(*endpoints.begin());
-            std::cerr << "SEGV" << std::endl;
             this->receive();
-            std::cerr << "SEGV" << std::endl;
             this->_recvThread = std::make_shared<std::thread>(std::thread([this] { this->_io_context->run(); }));
-            std::cerr << "SEGV" << std::endl;
             std::cerr << "\x1B[32m[DimensionClient]\x1B[0m: Connection to server established." << std::endl;
         } catch (std::exception &e) {
-            std::cerr << "-------------------------------------------------------------" << std::endl;
-            std::cerr << e.what() << std::endl;
-            std::cerr << "-------------------------------------------------------------" << std::endl;
+            std::cerr << "\x1B[31m[DimensionClient ERROR]\x1B[0m: "<< e.what() << std::endl;
         }
     }
 
