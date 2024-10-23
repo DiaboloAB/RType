570b3ee2c4df58dd0c29887d67ca0dd120fc1f90
Raphaël <raphael.mabille@epitech.eu>
2024-10-23 14:54:25 +0000

    build(sdl2): add sdl2 mixer and ttf to the build

--- conan/sdl/conanfile.py
+++ conan/sdl/conanfile.py
@@ -9,9 +9,11 @@ class MyProjectConan(ConanFile):
         self.requires("asio/1.31.0")
         self.requires("lua/5.4.7")
         self.requires("nlohmann_json/3.11.3")
-        self.requires("sdl/2.30.8")
         self.requires("sdl_ttf/2.22.0")
         self.requires("sdl_mixer/2.8.0")
+
+    def configure(self):
+        self.requires("sdl/2.28.5", override=True)
 
     def build_requirements(self):
         self.tool_requires("cmake/3.22.6")
