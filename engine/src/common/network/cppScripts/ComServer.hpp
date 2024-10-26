8f6c46fc114bf9dafb7db65cb9c9d1b33ecce0c8
maxence <maxence.largeot@epitech.eu>
2024-10-26 17:00:33 +0200

    feat(feature/dimNewE) : network files & ResendTimer struct

---
+++ engine/src/common/network/cppScripts/ComServer.hpp
@@ -0,0 +1,59 @@
+/**********************************************************************************
+ * Project: RType
+ * Description: A GAME ENGINE THAT ROARS !
+ * Author: Alexis BOITEL, Raphael MABILLE, Morgan LARGEOT, Maxence LARGEOT, Arthur DORIEL
+ * Date, Location: 2024, Rennes
+ **********************************************************************************/
+
+#ifndef COMSERVER_HPP
+#define COMSERVER_HPP
+
+#include <iostream>
+
+#include "ClientEventType.hpp"
+#include "common/ICppScript.hpp"
+#include "gameContext/GameContext.hpp"
+
+namespace RType
+{
+
+class ComServer : public RType::ICppScript
+{
+   public:
+    void update(mobs::Registry &registry, GameContext &gameContext) override
+    {
+        auto &networkC = registry.get<NetworkClient>(_entity);
+        auto _rcvQueue = networkC.client->getRcvQueue();
+        while (!_rcvQueue.empty())
+        {
+            auto packet = _rcvQueue.front();
+            auto validation = networkC.factory.createEmptyPacket<dimension::PacketValidation>();
+            validation->setPacketReceiveTimeStamp(packet.first->getPacketTimeStamp());
+            validation->setPacketReceiveType(packet.first->getPacketType());
+            networkC.client->send(validation, packet.second, false);
+            _rcvQueue.pop();
+            networkC.client->popReceiveQueue();
+        }
+        if (gameContext._runtime->getKeyDown(KeyCode::Enter))
+        {
+            networkC.client->connectServer("127.0.0.1", 8581);
+        }
+        if (gameContext._runtime->getKeyDown(KeyCode::R))
+        {
+            auto &networkC = registry.get<NetworkClient>(_entity);
+            auto event = networkC.factory.createEmptyPacket<dimension::ClientEvent>();
+            event->setClientEvent(dimension::ClientEventType::ROOM);
+            event->setDescription("cr=pv");
+            if (networkC.client->_serverEndpoint)
+                networkC.client->send(event, *networkC.client->_serverEndpoint);
+        }
+    }
+    void setEntity(mobs::Entity entity) override { _entity = entity; }
+
+   private:
+    mobs::Entity _entity;
+};
+
+}  // namespace RType
+
+#endif  // COMSERVER_HPP
