7e09a0a9803c0ec7ae2f7b2093a7e7139923a457
maxence <maxence.largeot@epitech.eu>
2024-10-31 14:46:53 +0100

    refactor(feature/netwInEcs): destroy handling

--- engine/src/common/network/cppScripts/redirect/EntityRedirect.hpp
+++ engine/src/common/network/cppScripts/redirect/EntityRedirect.hpp
@@ -32,13 +32,33 @@ class EntityRedirect {
                 networkData._id = packetCreate->getNetworkId();
                 LOG("EntityRedirect", "Entity created. {Network id: " + std::to_string(networkData._id) + "}");
             } catch (std::exception &e) {
-                ERR_LOG("EntityRedirect", std::string("{") + e.what() + "}");
+                ERR_LOG("EntityRedirect", std::string("create {") + e.what() + "}");
             }
         };
 
         static void destroy(mobs::Registry &registry, GameContext &gameContext, PacketDatas &packet)
         {
-            std::cerr << "Bonjour from destroy" << std::endl;
+            try {
+                auto packetDestroy = std::dynamic_pointer_cast<dimension::DestroyEntity>(packet.first);
+                uint64_t currentTime = std::chrono::duration_cast<std::chrono::seconds>(
+                               std::chrono::system_clock::now().time_since_epoch()).count();
+                if (packetDestroy->getPacketTimeStamp() + 2 >= currentTime)
+                    return;
+                mobs::Registry::View view = registry.view<NetworkData>();
+                uint32_t idToDestroy = packetDestroy->getNetworkId(); 
+                for (auto &entity : view) {
+                    auto &networkData = registry.get<NetworkData>(entity);
+                    if (networkData._id == idToDestroy) {
+                        registry.kill(entity);
+                        LOG("EntityRedirect", std::string("Entity destroyed {Network id: ") +
+                            std::to_string(idToDestroy) + "}");
+                        return;
+                    }
+                }
+
+            } catch (std::exception &e) {
+                ERR_LOG("EntityRedirect" , std::string("destroy {") + e.what() + "}");
+            }
         };
 
         static void move(mobs::Registry &registry, GameContext &gameContext, PacketDatas &packet)
